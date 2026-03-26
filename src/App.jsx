fa868708200be92d6474d896ba344e6c58a6f270
Sandip kushwaha <163518436+sandip-kushwaha@users.noreply.github.com>
2026-03-26 07:26:57 +0545

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import{ Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+import Contanct from './pages/Contact'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import Menubar from './components/menubar'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+const App = () => {
+  return (
+    <>
+      <ToastContainer position="top-right" autoClose={2000} />
+      <Navbar />
+      <Menubar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contact' element={<Contanct />} />
+        <Route path='/hire' element={<Contanct />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+export default App
