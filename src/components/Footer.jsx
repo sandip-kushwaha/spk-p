52b0aa0aaa8d3b7e3090c71cf9a8d76f44c5bd0e
Sandip kushwaha <163518436+sandip-kushwaha@users.noreply.github.com>
2026-03-28 07:57:11 +0545

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => {
           <h4>Web Developer</h4>
           <nav className="footer-items">
             <a href="/">Home</a>
-            <a href="/About">About</a>
-            <a href="/Projects">Projects</a>
-            <a href="/Contact">Contact</a>
+            <a href="/about">About</a>
+            <a href="/projects">Projects</a>
+            <a href="/contact">Contact</a>
           </nav>
         </div>
         <div className="social">
