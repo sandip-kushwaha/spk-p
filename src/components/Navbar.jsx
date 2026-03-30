370d54673a7685d63e63ac76b1d7341cbe233cb8
Sandip kushwaha <163518436+sandip-kushwaha@users.noreply.github.com>
2026-03-30 19:54:30 +0545

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import {FaLinkedin, FaGithub } from "react-icons/fa";
-import './Navbar.css'
+import './Navbar.css';
+
 
 const Navbar = () => {
   return (
